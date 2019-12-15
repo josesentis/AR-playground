08ec1afd1478d45d29486a5a5883fd771202e529
Jose Sentis <jose.sentis1987@gmail.com>
2019-12-15 19:41:17 +0100

    Updates

---
+++ precache-manifest.78fa297b0fb05426371d56459fd7d612.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f45e3314a620bbffe5cb574a78f92ef",
+    "url": "/AR-playground/index.html"
+  },
+  {
+    "revision": "b5eb91616fb2caac667d",
+    "url": "/AR-playground/static/js/2.c781fef3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/AR-playground/static/js/2.c781fef3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "174b0c32a6a55226ba23",
+    "url": "/AR-playground/static/js/main.2d8e98d6.chunk.js"
+  },
+  {
+    "revision": "3c9dd6c612067c91f3d4",
+    "url": "/AR-playground/static/js/runtime-main.0144b88c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e564009e2eaa03c2d663941d33197aac.js"
+  "/AR-playground/precache-manifest.78fa297b0fb05426371d56459fd7d612.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AR-playground/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
