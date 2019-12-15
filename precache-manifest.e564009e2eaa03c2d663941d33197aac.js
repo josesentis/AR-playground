95d50b1348f0174df62a8841f986f81eb09229aa
Jose Sentis <jose.sentis1987@gmail.com>
2019-12-15 19:35:43 +0100

    Updates

---
+++ precache-manifest.e564009e2eaa03c2d663941d33197aac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80ccd128736f6c62cf7375111597e09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5eb91616fb2caac667d",
+    "url": "/static/js/2.c781fef3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c781fef3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "581cc8c34093c779289c",
+    "url": "/static/js/main.c6d83294.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
