4bc3d28c62ecff562fdca8e321784ef9dc4b1425
Jose <jose@runroom.com>
2019-12-17 10:38:10 +0100

    Creates new scene with only text

--- src/components/boxScene/index.js
+++ src/components/boxScene/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-import './scene.css';
+import '../styles.css';
+
+const text = 'Some text.';
 
 const Scene = () => (
   <div className="scene">
     <a-scene embedded arjs='trackingMethod: best;'>
       <a-anchor hit-testing-enabled='true'>
         <a-box position='0 0.5 0' material='opacity: 0.5;'>
-        <a-entity position="1 0 0" scale="3 3 3" text="value: HELLO; color:black"></a-entity>
+          <a-entity position="1 0 0" scale="3 3 3" text={`value: ${text}; color: black;`}></a-entity>
         </a-box>
       </a-anchor>
       <a-camera-static/>
---
+++ src/components/textScene/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import '../styles.css';
+
+const text = 'Some text';
+
+const Scene = () => (
+  <div className="scene">
+    <a-scene embedded arjs='trackingMethod: best;'>
+      <a-assets>
+        <a-mixin id="text" text={`align: center; width: 3; font: https://cdn.aframe.io/fonts/Aileron-Semibold.fnt; value: ${text};`}></a-mixin>
+      </a-assets>
+      <a-marker type="barcode" value="51" smooth="true" smoothCount="10" smoothTolerance="0.005" smoothThreshold="1">
+        <a-text mixin="text" position="0 1 0" wrap-count="15" rotation="-90 0 0" color="blue"></a-text>
+      </a-marker>
+      <a-camera-static/>
+    </a-scene>
+  </div>
+);
+
+export default Scene;
