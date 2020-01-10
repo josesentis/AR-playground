c967c5455d4281c06641f27f1b83ee0afe981f20
Jose <jose@runroom.com>
2020-01-10 09:52:07 +0100

    Texting image positioj

--- src/pages/scanner/index.js
+++ src/pages/scanner/index.js
@@ -1,39 +1,39 @@
 import React, { useEffect, useState } from "react";
 
-// import ImageScene from '../../components/imageScene';
+import ImageScene from '../../components/imageScene';
 // import CustomScene from '../../components/customScene';
 // import HiroScene from '../../components/hiroScene';
-import TextScene from "../../components/textScene";
+// import TextScene from "../../components/textScene";
 
 import "../../styles.css";
-// import poster from './poster.png';
+import poster from './poster.png';
 // const text =
 // "This is the text that I'm displaying on the text box. This is how it should be long enought to be cut down";
-const colors = [
-  {
-    text: "first line of text",
-    color: "red"
-  },
-  {
-    text: "first line of text",
-    color: "blue"
-  },
-  {
-    text: "first line of text",
-    color: "yellow"
-  }
-];
+// const colors = [
+//   {
+//     text: "first line of text",
+//     color: "red"
+//   },
+//   {
+//     text: "first line of text",
+//     color: "blue"
+//   },
+//   {
+//     text: "first line of text",
+//     color: "yellow"
+//   }
+// ];
 
 const Scanner = () => {
   const [loaded, setLoaded] = useState(false);
-  // const [src, setSrc] = useState('');
-  const [content, setContent] = useState([]);
+  const [src, setSrc] = useState('');
+  // const [content, setContent] = useState([]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoaded(true);
-      // setSrc(poster);
-      setContent(colors);
+      setSrc(poster);
+      // setContent(colors);
     }, 1000);
 
     return () => clearTimeout(timer);
@@ -44,10 +44,10 @@ const Scanner = () => {
   return (
     <div className="scene">
       <a-scene embedded arjs="trackingMethod: best;">
-        {/* <ImageScene src={src} /> */}
+        <ImageScene src={src} />
         {/* <HiroScene /> */}
         {/* <CustomScene /> */}
-        <TextScene content={content} />
+        {/* <TextScene content={content} /> */}
         <a-camera-static />
       </a-scene>
     </div>
